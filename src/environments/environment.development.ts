4a21e39ea1de8d0715706b0ebf09f90a12bcaed2
genan2003 <genanomer99@gmail.com>
2024-07-26 02:42:27 +0300

    First commit- initial setup and auth

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wfninfeantfjmxtbzrfp.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmbmluZmVhbnRmam14dGJ6cmZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5NDU2ODQsImV4cCI6MjAzNzUyMTY4NH0.6XUvPjNnJRh55PpPPzqpvKXTLdKXtBTEWcfgNPzBV5o',
+};
